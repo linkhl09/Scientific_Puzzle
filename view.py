a0e4b2272532d9153b445f010600779fb6e78c76
linkhl09 <af.hernandezl@uniandes.edu.co>
2019-11-24 21:55:54 -0500

    Correction in textures, buttons

--- view.py
+++ view.py
@@ -3,8 +3,7 @@
 # Andrés Hernández: linkhl09
 # Felipe Parra: Parr0sky
 # -------------------------------------------------------------------------
-from builtins import bool
-from itertools import accumulate
+from tkinter import W
 
 import numpy as np
 import pygame
@@ -14,7 +13,6 @@ from pygame.locals import *
 # -------------------------------------------------------------------------
 # Constraints.
 # -------------------------------------------------------------------------
-
 
 
 # RGB Values
@@ -39,20 +37,21 @@ saddleBrown = (139, 69, 19)
 # Board init pos
 init_pos = np.array([15, 15])
 
-#timer survival
+# Timer survival
 counter, counterText = 30, '30'.rjust(3)
 pos_counter=[850,500]
 isSurvival=False
 
-#timer adventure
+# Timer adventure
 counterUp, counterUpText = 0, '0'.rjust(3)
 pos_counter=[850,500]
+
 # Button positions
 pos_btt3x3 = [790, 70]
 pos_btt4x4 = [850, 70]
 pos_btt5x5 = [910, 70]
-pos_btt_ma = [800, 170]
-pos_btt_md = [800, 230]
+pos_btt_ma = [790, 170]
+pos_btt_md = [790, 230]
 pos_btt_s1 = [790, 330]  # Fibonacci.
 pos_btt_s2 = [850, 330]  # Quadratic
 pos_btt_s3 = [910, 330]  # Primes
@@ -62,22 +61,8 @@ pos_btt_s6 = [910, 390]  # Odd Numbers
 
 # Text positions
 pos_b_s    = [875, 45]
-pos_txt3x3 = [815, 95]
-pos_txt4x4 = [875, 95]
-pos_txt5x5 = [935, 95]
-
 pos_mod    = [875, 150]
-pos_txt_ma = [875, 195]
-pos_txt_md = [875, 255]
-
 pos_series = [875, 310]
-pos_txt_s1 = [815, 355]  # Fibonacci.
-pos_txt_s2 = [875, 355]  # Quadratic
-pos_txt_s3 = [935, 355]  # Primes
-pos_txt_s4 = [815, 415]  # Powers of 2
-pos_txt_s5 = [875, 415]  # Even Numbers
-pos_txt_s6 = [935, 415]  # Odd Numbers
-
 
 # Font sizes.
 f_size_tit = 28
@@ -117,23 +102,35 @@ btt = pygame.image.load('Button.jpg')
 btt_s = pygame.image.load('Button size.jpg')
 
 space = pygame.image.load("game1.png")
-py_text=0
 # -------------------------------------------------------------------------
 # Aux methods
 # -------------------------------------------------------------------------
 
 
-def text_objects(text, font):
-    text_surface = font.render(text, True, White)
+def text_objects(text, font, color):
+    text_surface = font.render(text, True, color)
     return text_surface, text_surface.get_rect()
 
 
-def put_text(text, pos, f_size):
+def put_text(text, pos, f_size, color):
     py_text = pygame.font.Font('Roboto-Black.ttf', f_size)
-    text_surf, text_rect = text_objects(text, py_text)
+    text_surf, text_rect = text_objects(text, py_text, color)
     text_rect.center = pos
     surface.blit(text_surf, text_rect)
 
+
+def button(msg, texture, pos, w, h, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    surface.blit(texture, pos)
+    pos_txt = [pos[0]+np.floor(w/2), pos[1]+np.floor(h/2)]
+    if pos[0]+w > mouse[0] > pos[0] and pos[1]+h > mouse[1] > pos[1]:
+        put_text(msg, pos_txt, f_size_btt, Black)
+        if click[0] == 1:  # and action != None:
+            # action()
+            print("Esto es para la segunda entrega, que pereza")
+    else:
+        put_text(msg, pos_txt, f_size_btt, White)
 
 
 # -------------------------------------------------------------------------
@@ -147,8 +144,6 @@ while True:
             pygame.quit()
             sys.exit()
         else:
-
-
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -1
@@ -167,10 +162,13 @@ while True:
                     y_change = 0
                 elif event.key == pygame.K_DOWN:
                     y_change = 0
+
+    # Timer
     counter -= 0.0625
     counterText = str(int(counter)).rjust(3)+" sec" if counter > 0 else 'boom!'
     counterUp += 0.0625
     counterUpText = str(int(counterUp)).rjust(3) + " sec" if counter > 0 else 'boom!'
+
     # Set background
     surface.blit(bg, [0, 0])
 
@@ -194,45 +192,34 @@ while True:
             y = init_pos[1] + (j * act_size)
             act_pos = [x, y]
             surface.blit(space, act_pos)
-            put_text(world_matrix[i][j], [x + np.floor(act_size/2), y + np.floor(act_size/2)], act_f_size)
-
+            put_text(world_matrix[i][j], [x + np.floor(act_size/2), y + np.floor(act_size/2)], act_f_size, White)
 
     # Buttons board size
-    put_text("Board size", pos_b_s, f_size_tit)
-    surface.blit(btt_s, pos_btt3x3)
-    put_text("3x3", pos_txt3x3, f_size_btt)
-    surface.blit(btt_s, pos_btt4x4)
-    put_text("4x4", pos_txt4x4, f_size_btt)
-    surface.blit(btt_s, pos_btt5x5)
-    put_text("5x5", pos_txt5x5, f_size_btt)
+    put_text("Board size", pos_b_s, f_size_tit, White)
+    button("3x3", btt_s, pos_btt3x3, 50, 50)
+    button("4x4", btt_s, pos_btt4x4, 50, 50)
+    button("5x5", btt_s, pos_btt5x5, 50, 50)
 
     # Buttons game mode
-    put_text("Board size", pos_mod, f_size_tit)
-    surface.blit(btt, pos_btt_ma)
-    put_text("Adventure", pos_txt_ma, f_size_btt)
-    surface.blit(btt, pos_btt_md)
-    put_text("Challenge", pos_txt_md, f_size_btt)
+    put_text("Game mode", pos_mod, f_size_tit, White)
+    button("Adventure", btt, pos_btt_ma, 170, 50)
+    button("Challenge", btt, pos_btt_md, 170, 50)
 
     # Buttons series
-    put_text("Board size", pos_series, f_size_tit)
-    surface.blit(btt_s, pos_btt_s1)
-    put_text("Fib", pos_txt_s1, f_size_btt)
-    surface.blit(btt_s, pos_btt_s2)
-    put_text("X", pos_txt_s2, f_size_btt)
-    surface.blit(btt_s, pos_btt_s3)
-    put_text("Pri", pos_txt_s3, f_size_btt)
-    surface.blit(btt_s, pos_btt_s4)
-    put_text("2", pos_txt_s4, f_size_btt)
-    surface.blit(btt_s, pos_btt_s5)
-    put_text("Even", pos_txt_s5, f_size_btt)
-    surface.blit(btt_s, pos_btt_s6)
-    put_text("Odd", pos_txt_s6, f_size_btt)
+    put_text("Board size", pos_series, f_size_tit, White)
+    button("Fib", btt_s, pos_btt_s1, 50, 50)
+    button("X^2", btt_s, pos_btt_s2, 50, 50)
+    button("Pri", btt_s, pos_btt_s3, 50, 50)
+    button("2^n", btt_s, pos_btt_s4, 50, 50)
+    button("Even", btt_s, pos_btt_s5, 50, 50)
+    button("Odd", btt_s, pos_btt_s6, 50, 50)
 
+    # Timer txt
     if isSurvival:
-        put_text("Time remaining: ", pos_counter, f_size_tit)
-        put_text(counterText, (pos_counter[0], pos_counter[1] + 40), f_size_btt)
+        put_text("Time remaining: ", pos_counter, f_size_tit, White)
+        put_text(counterText, (pos_counter[0], pos_counter[1] + 40), f_size_btt, White)
     else:
-        put_text("Time Elapsed: ", pos_counter, f_size_tit)
-        put_text(counterUpText, (pos_counter[0], pos_counter[1] + 40), f_size_btt)
+        put_text("Time Elapsed: ", pos_counter, f_size_tit, White)
+        put_text(counterUpText, (pos_counter[0], pos_counter[1] + 40), f_size_btt, White)
     pygame.display.update()  # draws the Surface object returned by pygame.display.set_mode() to the screen
     clock.tick(60)  # 30 Frames per second
